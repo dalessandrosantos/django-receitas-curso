ebfdf7099642d8bba820b5a5e26d3a1104f48231
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-08-22 10:13:48 -0300

    adiciona pasta global_static

--- config/settings.py
+++ config/settings.py
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Arquivos globais
+STATICFILES_DIRS = [
+    BASE_DIR / 'global_static',
+]
+
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
