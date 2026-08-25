8e3f81c9baaf260083c0ebe3ccc95d4f3c0a6dea
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-08-24 22:19:31 -0300

    adiciona nova url e separa os templates de home e receita

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('receitas/', views.home, name="home")
+    path('', views.home, name="home"),
+    path('receita-view/', views.receitas, name='receitas'),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'receitas/paginas/home.html')
+
+def receitas(request):
+    return render(request, 'receitas/paginas/receita-view.html')
