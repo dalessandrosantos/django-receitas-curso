6fb8e99b77bf1c8d9ed77766f6a297d24349c2f3
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-08-25 23:21:58 -0300

    Criando o block para o título da página

--- receitas/urls.py
+++ receitas/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    path('receita-view/', views.receitas, name='receitas'),
+    path('receitas/<int:id>/', views.receita, name='receita'),
 ]
--- receitas/views.py
+++ receitas/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'receitas/paginas/home.html')
 
-def receitas(request):
-    return render(request, 'receitas/paginas/receita-view.html')
+def receita(request, id):
+    context = {
+        'name': 'Dalessandro'
+    }
+    return render(request, 'receitas/paginas/receita-view.html', context)
