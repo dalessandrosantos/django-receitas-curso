f037c1b6ceed185f60403bbeced61c5187ad3299
Dalessandro Santos <dalessandrosantos83@gmail.com>
2026-09-01 14:07:34 -0300

    Ocultando receitas que não estão publicadas

--- receitas/views.py
+++ receitas/views.py
@@ -3,14 +3,19 @@ from utils.receitas.factory import make_recipe
 from .models import Receita
 
 def home(request):
-    receitas = Receita.objects.all().order_by('-id')
+    receitas = Receita.objects.filter(
+        publicado=True
+    ).order_by('-id')
     return render(request, 'receitas/paginas/home.html', context={
         'receitas': receitas,
     })
 
 def categoria(request, categoria_id):
-    receitas = Receita.objects.filter(categoria__id=categoria_id).order_by('-id')
-    return render(request, 'receitas/paginas/home.html', context={
+    receitas = Receita.objects.filter(
+        categoria__id=categoria_id,
+        publicado=True
+    ).order_by('-id')
+    return render(request, 'receitas/paginas/categoria.html', context={
         'receitas': receitas,
     })
 
